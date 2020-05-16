1d44bf94f80aa0c294277f71d754c2a8c97cc47f
Mark Iliev <mark.iliev@headworks.com.ua>
2020-05-16 18:25:08 +0300

    Added base behaviour and refactor csprojes

---
+++ src/BaseBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace Basil.Behaviors
+{
+    public abstract class BaseBehavior : Behavior
+    {
+        protected BindableObject AssociatedObject { get; private set; }
+
+        #region Overrides
+
+        protected override void OnAttachedTo(BindableObject bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            AssociatedObject = bindable;
+        }
+
+        protected override void OnDetachingFrom(BindableObject bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            AssociatedObject = null;
+        }
+
+        #endregion
+    }
+}
