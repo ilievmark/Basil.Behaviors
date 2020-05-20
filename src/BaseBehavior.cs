0b1ed337e50ac2dc591bad8d030428ab2e0783c5
Mark Iliev <mark.iliev@headworks.com.ua>
2020-05-20 21:47:21 +0300

    Upgrade to xamarin 4.6 & fix problem with binding context

--- src/BaseBehavior.cs
+++ src/BaseBehavior.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Basil.Behaviors
 {
@@ -11,17 +12,32 @@ namespace Basil.Behaviors
         protected override void OnAttachedTo(BindableObject bindable)
         {
             base.OnAttachedTo(bindable);
-
             AssociatedObject = bindable;
+
+            if (bindable.BindingContext != null)
+                BindingContext = bindable.BindingContext;
+
+            bindable.BindingContextChanged += OnBindingContextChanged;
         }
 
         protected override void OnDetachingFrom(BindableObject bindable)
         {
             base.OnDetachingFrom(bindable);
-
+            bindable.BindingContextChanged -= OnBindingContextChanged;
             AssociatedObject = null;
         }
 
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+
         #endregion
+
+        private void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
     }
 }
