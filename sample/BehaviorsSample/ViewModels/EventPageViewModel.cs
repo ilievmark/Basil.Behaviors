4fb2198c0a21c326cc77c31a0249bb88d53854dd
Mark Iliev <mark.iliev@headworks.com.ua>
2020-07-08 22:19:33 +0300

    Added return typed parameter with demos

--- sample/BehaviorsSample/ViewModels/EventPageViewModel.cs
+++ sample/BehaviorsSample/ViewModels/EventPageViewModel.cs
@@ -9,20 +9,23 @@ namespace BehaviorsSample.ViewModels
     public class EventPageViewModel : BaseViewModel
     {
         public ICommand Sample1Command => new Command(OnSample1Command);
-
         private void OnSample1Command()
         {
             Debug.WriteLine("Hello from Sample1Command");
         }
 
         public ICommand Sample2Command => new Command(OnSample2Command);
-
         private void OnSample2Command()
         {
             Debug.WriteLine("Hello from Sample2Command");
         }
         
-        public void JustAMethodNoMore(string param)
+        public string ReturnStringMethod()
+        {
+            return "Hello string from ReturnStringMethod";
+        }
+        
+        protected void JustAMethodNoMore(string param)
         {
             Debug.WriteLine($"Hello from JustAMethodNoMore {param}");
         }
--- src/Events/Parameters/ReturnParameter.cs
+++ src/Events/Parameters/ReturnParameter.cs
@@ -4,12 +4,16 @@ using Xamarin.Forms;
 namespace Basil.Behaviors.Events.Parameters
 {
     [ContentProperty(nameof(Value))]
-    public sealed class ReturnParameter : Parameter
+    public abstract class ReturnParameter : Parameter
     {
         public object Value { get; set; }
-
-        public override Type GetParamType() => Value.GetType();
+        
         public override object GetValue() => Value;
         public void SetValue(object value) => Value = value;
+    }
+    
+    public sealed class ReturnParameter<T> : ReturnParameter
+    {
+        public override Type GetParamType() => typeof(T);
     }
 }
