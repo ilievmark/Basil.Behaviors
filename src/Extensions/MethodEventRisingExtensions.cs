28c9d92aa69b9372cf6936f8c003768e81350589
Mark Iliev <mark.iliev@headworks.com.ua>
2020-07-11 00:00:18 +0300

    Added supporting pathes for methods handlers

--- src/Extensions/MethodEventRisingExtensions.cs
+++ src/Extensions/MethodEventRisingExtensions.cs
@@ -16,10 +16,13 @@ namespace Basil.Behaviors.Extensions
         
         public static object ExecuteMethod(this EventToMethodHandler handler)
         {
-            var target = handler.TargetObject ?? handler.AssociatedObject?.BindingContext;
+            var target = handler.TargetObject ?? handler.AssociatedObject?.BindingContext ?? handler.AssociatedObject;
 
             if (target == null)
                 throw new InvalidDataException();
+
+            if (!string.IsNullOrEmpty(handler.MethodName) && handler.MethodName.ContainsChar('.'))
+                return target.RunMethodPath(handler.MethodName, handler.Parameters);
             
             return target.RunMethod(handler.MethodName, handler.Parameters);
         }
@@ -32,10 +35,14 @@ namespace Basil.Behaviors.Extensions
 
         public static object ExecuteMethod(this EventToMethodBehavior behavior)
         {
-            var target = behavior.TargetObject ?? behavior.AssociatedObject?.BindingContext;
+            var target = behavior.TargetObject ?? behavior.AssociatedObject?.BindingContext ?? behavior.AssociatedObject;
 
             if (target == null)
                 throw new InvalidDataException();
+            
+            
+            if (!string.IsNullOrEmpty(behavior.MethodName) && behavior.MethodName.ContainsChar('.'))
+                return target.RunMethodPath(behavior.MethodName, behavior.Parameters);
             
             return target.RunMethod(behavior.MethodName, behavior.Parameters);
         }
