321be4d6e8413cae72bb4b133debef0f0c35fc28
Mark Iliev <mark.iliev@headworks.com.ua>
2020-09-12 12:31:15 +0300

    Added cancellation and reflection parameters

--- src/Events/Parameters/ReturnParameter.cs
+++ src/Events/Parameters/ReturnParameter.cs
@@ -1,19 +1,24 @@
 using System;
+using Basil.Behaviors.Events.Parameters.Abstract;
 using Xamarin.Forms;
 
 namespace Basil.Behaviors.Events.Parameters
 {
     [ContentProperty(nameof(Value))]
-    public abstract class ReturnParameter : Parameter
+    public abstract class ReturnParameter : Parameter, IReturnable
     {
         public object Value { get; set; }
         
-        public override object GetValue() => Value;
-        public void SetValue(object value) => Value = value;
+        public override object GetValue()
+            => Value;
+        
+        public void SetValue(object value)
+            => Value = value;
     }
     
     public class ReturnParameter<T> : ReturnParameter
     {
-        public override Type GetParamType() => typeof(T);
+        public override Type GetParamType()
+            => typeof(T);
     }
 }
