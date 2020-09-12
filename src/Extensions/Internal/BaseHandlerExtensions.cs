25bfaf925cc47acea689a4f92b27aa8a33cc7ec2
Mark Iliev <mark.iliev@headworks.com.ua>
2020-09-12 13:43:45 +0300

    Added base handler extensions

--- src/Extensions/Internal/BaseHandlerExtensions.cs
+++ src/Extensions/Internal/BaseHandlerExtensions.cs
@@ -1,67 +1,199 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Basil.Behaviors.Events.HandlerAbstract;
 using Basil.Behaviors.Events.HandlerBase;
 using Basil.Behaviors.Events.Parameters;
+using Basil.Behaviors.Events.Parameters.Abstract;
 using Xamarin.Forms.Internals;
 
 namespace Basil.Behaviors.Extensions.Internal
 {
     public static class BaseHandlerExtensions
     {
-        internal static void SetReturnParameter(this BaseHandler handler, object returnValue)
-        {
-            handler.SetReturnParameterToParametrizedHandler(returnValue);
-            handler.SetReturnParameterToCompositeHandler(returnValue);
-            handler.SetReturnParameterToCompositeParallelHandler(returnValue);
-        }
+        #region Cancellation
 
-        private static void SetReturnParameterToCompositeParallelHandler(this BaseHandler handler, object returnValue)
-        {
-            if (handler is ICompositeParallelHandler castedHandler)
-                foreach (var innerHandler in castedHandler.GetInnerHandlers())
-                    innerHandler.SetReturnParameter(returnValue);
-        }
+        #endregion
         
-        private static void SetReturnParameterToCompositeHandler(this BaseHandler handler, object returnValue)
-        {
-            if (handler is ICompositeHandler castedHandler)
-            {
-                var innerHandler = castedHandler.GetInnerHandlers().FirstOrDefault();
+        #region Multiple rising handlers
 
-                while (innerHandler.IsSkipReturnable())
-                {
-                    innerHandler = castedHandler.GetInnerHandlers().GetNextElement(innerHandler);
-                }
+        public static async Task RunSequentiallyAsync(
+            this IList<BaseHandler> handlers,
+            object sender,
+            object eventArgs)
+        {
+            object previousResult;
+
+            for (int i = 0; i < handlers.Count; i++)
+            {
+                var handler = handlers[i];
+                previousResult = default;
+
+                if (handler.IsAsyncGenericRisible())
+                    previousResult = await handler.RiseAsAsyncGeneric(sender, eventArgs);
                 
-                innerHandler.SetReturnParameterToParametrizedHandler(returnValue);
-                innerHandler.SetReturnParameterToCompositeHandler(returnValue);
-                innerHandler.SetReturnParameterToCompositeParallelHandler(returnValue);
+                if (handler.IsAsyncRisible())
+                    previousResult = await handler.RiseAsAsync(sender, eventArgs);
+
+                if (handler.IsGenericRisible())
+                    previousResult = handler.RiseAsGeneric(sender, eventArgs);
+                
+                if (handler.IsRisible())
+                    handler.RiseByDefault(sender, eventArgs);
+
+                if (previousResult != default)
+                    handlers.GetNextParametrizedHandler(i)?.SetReturnValueToNext(previousResult);
             }
         }
-
-        private static void SetReturnParameterToParametrizedHandler(this BaseHandler handler, object returnValue)
+        
+        public static BaseHandler GetNextParametrizedHandler(this IList<BaseHandler> handlers, int index)
         {
-            if (handler is IParametrisedHandler castedHandler)
-                castedHandler.SetReturnParameterToParametrizedHandler(returnValue);
+            BaseHandler handler;
+            do handler = ++index >= handlers.Count ? null : handlers[index];
+            while (handler.IsSkipReturnable());
+            return handler;
+        }
+        
+        #endregion
+        
+        #region Rising handlers
+
+        public static void RiseByDefault(
+            this BaseHandler handler,
+            object sender,
+            object eventArgs)
+        {
+            if (handler.Cast<IRisible>(out var castedRisible))
+                castedRisible.Rise(sender, eventArgs);
+        }
+        
+        public static object RiseAsGeneric(
+            this BaseHandler handler,
+            object sender,
+            object eventArgs)
+        {
+            object returnValue = default;
+
+            if (handler.Cast<IGenericRisible>(out var castedGenericHandler))
+                returnValue = castedGenericHandler.Rise<object>(sender, eventArgs);
+
+            return returnValue;
+        }
+        
+        public static async Task<object> RiseAsAsync(
+            this BaseHandler handler,
+            object sender,
+            object eventArgs)
+        {
+            object returnValue = default;
+
+            if (handler.Cast<IAsyncRisible>(out var castedAsyncHandler))
+            {
+                if (castedAsyncHandler.WaitResult)
+                    await castedAsyncHandler.RiseAsync(sender, eventArgs);
+                else
+                    returnValue = castedAsyncHandler.RiseAsync(sender, eventArgs);
+            }
+
+            return returnValue;
+        }
+        
+        public static async Task<object> RiseAsAsyncGeneric(
+            this BaseHandler handler,
+            object sender,
+            object eventArgs)
+        {
+            object returnValue = default;
+            
+            if (handler.Cast<IAsyncGenericRisible>(out var castedAsyncGenericHandler))
+            {
+                if (castedAsyncGenericHandler.WaitResult)
+                {
+                    var task = castedAsyncGenericHandler.RiseAsync(sender, eventArgs);
+                    await task;
+                    returnValue = task.GetType().GetProperty(nameof(Task<object>.Result)).GetValue(task);
+                }
+                else
+                {
+                    returnValue = castedAsyncGenericHandler.RiseAsync(sender, eventArgs);
+                }
+            }
+
+            return returnValue;
+        }
+        
+        #endregion
+        
+        #region Return parameter managing
+        
+        public static void SetReturnValueToNext(this BaseHandler handler, object returnValue)
+        {
+            handler.SetReturnValueIfParametrizedHandler(returnValue);
+            handler.SetReturnValueIfCompositeHandler(returnValue);
+            handler.SetReturnValueIfCompositeParallelHandler(returnValue);
         }
 
-        private static void SetReturnParameterToParametrizedHandler(this IParametrisedHandler handler, object returnValue)
+        public static void SetReturnValueIfCompositeParallelHandler(this BaseHandler handler, object returnValue)
+        {
+            if (handler.Cast<ICompositeParallelHandler>(out var castedHandler))
+                foreach (var innerHandler in castedHandler.GetInnerHandlers())
+                    innerHandler.SetReturnValueToNext(returnValue);
+        }
+        
+        public static void SetReturnValueIfCompositeHandler(this BaseHandler handler, object returnValue)
+        {
+            if (handler.Cast<ICompositeHandler>(out var castedHandler))
+                castedHandler.GetNextNotSkipReturnable().SetReturnValueToNext(returnValue);
+        }
+
+        public static void SetReturnValueIfParametrizedHandler(this BaseHandler handler, object returnValue)
+        {
+            if (handler.Cast<IParametrisedHandler>(out var castedHandler))
+                castedHandler.SetReturnValue(returnValue);
+        }
+
+        #endregion
+        
+        #region Interface logic
+        
+        public static void SetReturnValue(this IParametrisedHandler handler, object returnValue)
         {
             handler?
                 .GetParameters()
-                .OfType<ReturnParameter>()
+                .OfType<IReturnable>()
                 .ForEach(
-                    p => p.SetValue(returnValue));
+                    p => p.SetReturnValue(returnValue));
         }
-        
-        private static T GetNextElement<T>(this IList<T> collection, T current)
+
+        public static BaseHandler GetNextNotSkipReturnable(this ICompositeHandler compositeHandler)
         {
-            var index = collection.IndexOf(current);
-            if (index + 1 < collection.Count)
-                return collection[index + 1];
-            return default;
+            var handlers = compositeHandler.GetInnerHandlers();
+            var innerHandler = handlers.FirstOrDefault();
+
+            while (innerHandler.IsSkipReturnable())
+                innerHandler = handlers.GetNextFrom(innerHandler);
+
+            return innerHandler;
         }
+
+        #endregion
+        
+        #region Check type and cast
+        
+        public static bool IsRisible(this BaseHandler handler)
+            => handler is IRisible;
+
+        public static bool IsAsyncRisible(this BaseHandler handler)
+            => handler is IAsyncRisible;
+
+        public static bool IsGenericRisible(this BaseHandler handler)
+            => handler is IGenericRisible;
+
+        public static bool IsAsyncGenericRisible(this BaseHandler handler)
+            => handler is IAsyncGenericRisible;
         
         public static bool IsParametrised(this BaseHandler handler)
             => handler is IParametrisedHandler;
@@ -74,5 +206,7 @@ namespace Basil.Behaviors.Extensions.Internal
 
         public static bool IsSkipReturnable(this BaseHandler handler)
             => handler is ISkipReturnable;
+        
+        #endregion
     }
 }
