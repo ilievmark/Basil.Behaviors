c308b36e178c3766b18633392ed3d77f5d26efde
Mark Iliev <mark.iliev@headworks.com.ua>
2020-09-12 13:43:06 +0300

    Update object reflection extensions

--- src/Extensions/Internal/ObjectReflectionExtension.cs
+++ src/Extensions/Internal/ObjectReflectionExtension.cs
@@ -7,39 +7,60 @@ namespace Basil.Behaviors.Extensions.Internal
 {
     public static class ObjectReflectionExtension
     {
+        public static bool Cast<TCastType>(this object castable, out TCastType castedValue)
+            where TCastType : class
+            => (castedValue = castable as TCastType) != null;
+        
         public static TProperty GetPropertyValue<TProperty>(this object target, string propertyName)
-            => (TProperty) target?.GetPropertyInfo(propertyName)?.GetValue(target);
+            => (TProperty) target.GetPropertyValue(propertyName);
         
         public static object GetPropertyValue(this object target, string propertyName)
-            => target?.GetPropertyInfo(propertyName)?.GetValue(target);
-        
+            => target.GetPropertyInfo(propertyName).GetValue(target);
+
         public static TField GetFieldValue<TField>(this object target, string fieldName)
-            => (TField) target?.GetFieldInfo(fieldName)?.GetValue(target);
+            => (TField) target.GetFieldValue(fieldName);
         
         public static object GetFieldValue(this object target, string fieldName)
-            => target?.GetFieldInfo(fieldName)?.GetValue(target);
+            => target.GetFieldInfo(fieldName).GetValue(target);
 
         public static FieldInfo GetFieldInfo(this object target, string fieldName)
         {
-            ValidateTargets(target, fieldName);
+            target.ValidateObject(nameof(target));
+            fieldName.ValidateString(nameof(fieldName));
 
-            return target.GetType().GetRuntimeFields().FirstOrDefault(f => f.Name == fieldName);
+            var info = target.GetType().GetRuntimeFields().FirstOrDefault(f => f.Name == fieldName);
+            info.ValidateMember(fieldName);
+
+            return info;
         }
         
         public static PropertyInfo GetPropertyInfo(this object target, string propertyName)
         {
-            ValidateTargets(target, propertyName);
+            target.ValidateObject(nameof(target));
+            propertyName.ValidateString(nameof(propertyName));
 
-            return target.GetType().GetProperties().FirstOrDefault(p => p.Name == propertyName);
+            var info = target.GetType().GetProperties().FirstOrDefault(p => p.Name == propertyName);
+            info.ValidateMember(propertyName);
+            
+            return info;
         }
 
-        private static void ValidateTargets(object target, string name)
+        internal static void ValidateObject(this object obj, string valueName = null)
         {
-            if (target == null)
-                throw new ArgumentNullException(nameof(target));
+            if (obj == null)
+                throw new ArgumentNullException(valueName);
+        }
 
-            if (string.IsNullOrEmpty(name))
-                throw new ArgumentNullException(nameof(name));
+        internal static void ValidateString(this string str, string valueName = null)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentNullException(valueName);
+        }
+        
+        internal static void ValidateMember(this MemberInfo member, string memberName)
+        {
+            if(member == null)
+                throw new MissingMemberException(memberName);
         }
     }
 }
