a049554be90538491eac48c3b6564e566971e726
Mark Iliev <mark.iliev@headworks.com.ua>
2020-09-13 10:53:08 +0300

    Refactored IAsyncRisible and generic version

--- src/Events/HandlerAbstract/IAsyncGenericRisible.cs
+++ src/Events/HandlerAbstract/IAsyncGenericRisible.cs
@@ -2,8 +2,10 @@ using System.Threading.Tasks;
 
 namespace Basil.Behaviors.Events.HandlerAbstract
 {
-    public interface IAsyncGenericRisible : IAsyncRisible
+    public interface IAsyncGenericRisible
     {
+        bool WaitResult { get; }
+
         Task<T> RiseAsync<T>(object sender, object eventArgs);
     }
 }
